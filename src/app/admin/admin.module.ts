b51ebbd11eb71a52f7b31f1af9dd96a53e424905
Dima Khomko <khomko.dima@gmail.com>
2020-04-01 10:57:50 +0300

    настройка fb

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,7 +8,6 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-
 import {AuthService} from './shared/service/auth.service';
 import {SharedModule} from '../shared/shared.module';
 
